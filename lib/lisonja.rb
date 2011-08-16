b90cc468ef6967d24098f025d8ce9f03b186aae3
David Calavera & Jacob Burkhart <dcalavera@engineyard.com>
2011-08-16 12:16:40 -0700

    update to latest ey_api_hmac (use slightly changed SSO api), update README with details of SSO implementation

--- lib/lisonja.rb
+++ lib/lisonja.rb
@@ -212,7 +212,7 @@ EOT
     end
 
     get "/sso/customers/:customer_id" do |customer_id|
-      raise "Signature invalid" unless EY::ApiHMAC.verify_for_sso(request.url, Lisonja.api_creds.auth_id, Lisonja.api_creds.auth_key)
+      raise "Signature invalid" unless EY::ApiHMAC::SSO.authenticated?(request.url, Lisonja.api_creds.auth_id, Lisonja.api_creds.auth_key)
       @customer = Lisonja::Customer.find(customer_id)
       @redirect_to = params[:ey_return_to_url]
       haml :plans
@@ -242,7 +242,7 @@ EOT
   
     get "/sso/customers/:customer_id/generators/:generator_id" do |customer_id, generator_id|
       #TODO: use a signature verification middleware instead?
-      raise "Signature invalid" unless EY::ApiHMAC.verify_for_sso(request.url, Lisonja.api_creds.auth_id, Lisonja.api_creds.auth_key)
+      raise "Signature invalid" unless EY::ApiHMAC::SSO.authenticated?(request.url, Lisonja.api_creds.auth_id, Lisonja.api_creds.auth_key)
       @customer = Lisonja::Customer.find(customer_id)
       @generator = @customer.compliment_generators.find(generator_id)
       @redirect_to = params[:ey_return_to_url]
