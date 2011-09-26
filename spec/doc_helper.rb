5a6dd22b0d90108b4db93dd90b069f12b87e29c1
Jim Lindley & Jason Hansen <jlindley@engineyard.com>
2011-09-26 11:04:39 -0700

    WIP

--- spec/doc_helper.rb
+++ spec/doc_helper.rb
@@ -1,6 +1,15 @@
 class DocHelper
 
-  def self.save(key, string)
+  def self.save(key, obj)
+    string = obj.inspect
+    if obj.is_a?(Hash)
+      max_key_length = obj.keys.sort_by{|k| k.to_s.length}.last.to_s.length
+      string.gsub!(", :", ", \n  :")
+      string.gsub!("{", "{\n  ")
+      string.gsub!("}", "\n}")
+      string.gsub!("=>", " => ")
+      obj.keys.each{|k| string.gsub!(":#{k}", ":#{k.to_s.ljust(max_key_length)}") }
+    end
     snippets[key] = string
   end
 
--- spec/ey_integration/setup_spec.rb
+++ spec/ey_integration/setup_spec.rb
@@ -16,7 +16,7 @@ describe "credentials" do
 #{save_creds_call{
       Chronatog::EyIntegration.save_creds('ff4d04dbea52c605', 'e301bcb647fc4e9def6dfb416722c583cf3058bc1b516ebb2ac99bccf7ff5c5ea22c112cd75afd28')
 #}save_creds_call}
-    DocHelper.save('save_creds_result', save_creds_result.inspect)
+    DocHelper.save('save_creds_result', save_creds_result)
     Chronatog::EyIntegration.api_creds.should_not be_nil
     Chronatog::EyIntegration.api_creds.auth_id.should eq "ff4d04dbea52c605"
     Chronatog::EyIntegration.api_creds.auth_key.should eq "e301bcb647fc4e9def6dfb416722c583cf3058bc1b516ebb2ac99bccf7ff5c5ea22c112cd75afd28"
