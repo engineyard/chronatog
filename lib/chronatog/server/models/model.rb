9dca3d6e201cc6f7c0844398c311acc490477120
Jacob Burkhart <jburkhart@engineyard.com>
2011-10-06 15:59:32 -0700

    workaround for bug in active record 3.1 (https://github.com/rails/rails/pull/3240)

--- lib/chronatog/server/models/model.rb
+++ lib/chronatog/server/models/model.rb
@@ -3,9 +3,18 @@ require 'active_record'
 module Chronatog
   module Server
     class Model < ActiveRecord::Base
+      CONNECTION_ARGS = (defined?(CHRONOS_DB_CREDS) && CHRONOS_DB_CREDS) || {
+        :adapter  => "sqlite3",
+        :database => File.expand_path("../../../../../tmp/chronatog.sqlite3", __FILE__)
+      }
       @abstract_class = true
-      establish_connection(:adapter  => "sqlite3",
-                           :database => File.expand_path("../../../../../tmp/chronatog.sqlite3", __FILE__))
+      establish_connection(CONNECTION_ARGS)
+
+      #WORKAROUND fix for bug in AR 3.1 (https://github.com/rails/rails/pull/3240)
+      unless ActiveRecord::Base.connected?
+        ActiveRecord::Base.establish_connection(CONNECTION_ARGS)
+      end
+
     end
   end
 end
--- spec/server/spec_helper.rb
+++ spec/server/spec_helper.rb
@@ -5,6 +5,6 @@ require File.join( File.dirname(__FILE__), "../doc_helper" )
 shared_context "chronatog server reset" do
   before(:each) do
     Chronatog::Server.reset!
-    ActiveRecord::Base.descendants.each(&:reset_column_information)
+    Chronatog::Server::Model.descendants.each(&:reset_column_information)
   end
 end
